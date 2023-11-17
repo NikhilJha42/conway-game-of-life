98c69097077943e70d8fe171eeaaa5c2ec2b485f
Nikhil <njha@spartaglobal.com>
2023-11-17 12:31:49 +0000

    GameOfLife updates its state depending on which squares are selected.

--- app/GameOfLife.tsx
+++ app/GameOfLife.tsx
@@ -1,40 +1,104 @@
-import { Key } from 'react';
-import './game-of-life.css'
-export default function GameOfLife() {
-    return (
-        <div>
-            <GameTable height={100} width={100}/>
-        </div>
-    )
-}
+"use client";
 
-function GameTable({height, width}: any) {
-    const rows = [];
+import "./game-of-life.css";
+import { useState } from "react";
+export default function GameOfLife({ height, width }: any) {
+  const initalConfig: any = [];
+  for (let i = 0; i < height; i++) {
+    initalConfig.push([]);
+    for (let j = 0; j < width; j++) {
+      initalConfig[i].push(false);
+    }
+  }
+  const [config, setConfig] : any = useState(initalConfig);
+
+  const [display, setDisplay] : any = useState(0);
+
+  function updateDisplay() {
+    let counter : number = 0;
     for(let i=0; i < height; i++){
-        rows.push(<GameRow rowNum={i} rowLen={width}/>)
+        for(let j = 0; j < width; j++) {
+            if(config[i][j]){
+                counter++;
+            }
+        }
     }
-    return(
-        <table className='game-of-life'>
-            <tbody>
-                {rows}
-            </tbody>
-        </table>
-    )
+    setDisplay(counter);
+  }
+  function updateConfig(l: any, k: any, input: any) {
+
+    config[l][k] = input;
+    setConfig(config);
+
+    updateDisplay();
+  }
+
+
+  return (
+    <div>
+      <GameTable
+        updateConfig={updateConfig}
+        activeStates={config}
+        height={height}
+        width={width}
+      />
+      {/* <PlayButton updateConfig={updateConfig}/> */}
+      <p>{display}</p>
+    </div>
+  );
 }
 
-function GameRow({rowNum, rowLen}:any) {
-    const cells = [];
-    for(let i=0; i < rowLen; i++){
-        const cellId = "R" + rowNum + "C" + i;
-        cells.push(<Cell id={cellId}/>)
-    }
-    return(
-        <tr>{cells}</tr>
-    )
+function PlayButton({ updateConfig }: any) {
+  return <button onClick={() => updateConfig(0, 0, 1)}>Play</button>;
 }
 
-function Cell({id}:any) {
-    return(
-        <td className="cell" id={id}></td>
-    )
+function GameTable({ height, width, activeStates, updateConfig }: any) {
+  const rows = [];
+  for (let i = 0; i < height; i++) {
+    rows.push(
+      <GameRow
+        rowNum={i}
+        rowLen={width}
+        activeStates={activeStates[i]}
+        updateConfig={updateConfig}
+      />
+    );
+  }
+  return (
+    <table className="game-of-life">
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
+
+function GameRow({ rowNum, rowLen, activeStates, updateConfig }: any) {
+  const cells = [];
+  for (let i = 0; i < rowLen; i++) {
+    const cellId = "R" + rowNum + "C" + i;
+    cells.push(
+      <Cell
+        id={cellId}
+        position={[rowNum, i]}
+        activeState={activeStates[i]}
+        updateConfig={updateConfig}
+      />
+    );
+  }
+  return <tr>{cells}</tr>;
+}
+
+function Cell({ id, position, activeState, updateConfig }: any) {
+  const [state, setState] = useState(activeState);
+  const activeClass = state ? " " + "active" : "";
+
+  function handleClick() {
+    const newState : boolean = !state;
+    setState(newState);
+    updateConfig(...position, newState);
+  }
+  return (
+    <td className={"cell" + activeClass} id={id} onClick={() => handleClick()}>
+      <input type="radio" />
+    </td>
+  );
 }
