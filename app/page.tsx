9c323e09b499f600c8f17b9b363f4a6e6025b1ac
Nikhil <njha@spartaglobal.com>
2024-05-01 21:17:54 +0100

    Spelling correction.

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <Header 
           title={"Conway's Game of Life Simulator"} 
           description={`
-            Use this simulator to observe the beatuiful patterns 
+            Use this simulator to observe the beautiful patterns 
             that arise from an initial configuration of live cells 
             - simply click to activate or deactivate the cells, then 
             press play. Watch your still lifes remain still, 
