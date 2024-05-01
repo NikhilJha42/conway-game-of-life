b1db64a74cf69dc6c3839b47ba14071cafab2ff2
Nikhil <njha@spartaglobal.com>
2024-05-01 21:14:15 +0100

    Description update.

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
           description={`
             Use this simulator to observe the beatuiful patterns 
             that arise from an initial configuration of live cells 
-            - simply click to activate or deactivate the cells. 
-            Then watch your still lifes remain still, 
+            - simply click to activate or deactivate the cells, and 
+            press play. Then watch your still lifes remain still, 
             oscillators oscillate, and spaceships fly!
           `}/>
         <GameOfLife height={20} width={20}/>
